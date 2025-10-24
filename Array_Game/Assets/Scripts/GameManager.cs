ffc75ccc1d4c7c3a5cb69fc8ecf48f503f378004
Fides37 <fwong5@pratt.edu>
2025-10-24 13:16:40 -0400

    added game manager script

---
+++ Array_Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject RockCard;
+    public GameObject PaperCard;
+    public GameObject ScissorCard;
+
+    public GameObject[] CardDeck;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void ShuffleDeck(GameObject[] card)
+    {
+        
+    }
+
+
+}
