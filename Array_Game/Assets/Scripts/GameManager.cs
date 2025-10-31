431cc9b9196503e44bd8e308c3863d671f46e51c
Fides37 <fwong5@pratt.edu>
2025-10-31 11:02:55 -0400

    random updates

--- Array_Game/Assets/Scripts/GameManager.cs
+++ Array_Game/Assets/Scripts/GameManager.cs
@@ -8,26 +8,48 @@ public class GameManager : MonoBehaviour
     public GameObject PaperCard;
     public GameObject ScissorCard;
 
-    public GameObject[] CardDeck;
+    public GameObject [] deck = new GameObject [10];
+
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ShuffleDeck(deck);
+   
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        
+
     }
 
 
-    private void ShuffleDeck(GameObject[] card)
+    private void ShuffleDeck(GameObject[] deck)
+    {
+        for (int cardIndex = 0; cardIndex < deck.Length; cardIndex++)
+        {
+            Debug.Log(deck[cardIndex].name);
+
+            GameObject tempCard = deck[cardIndex];
+            int newIndex = Random.Range(cardIndex, deck.Length);
+            deck[cardIndex] = deck[newIndex];
+            deck[newIndex] = tempCard;
+
+
+        }
+
+
+    }
+
+
+    private void MoveCards()
     {
         
     }
-
 
 }
