75e99ecab0b566e3228e0fa44be7bdbabc79092a
Fides37 <fwong5@pratt.edu>
2025-11-06 19:41:50 -0500

    made changes to scripts

---
+++ Array_Game/Assets/CardMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardMovement : MonoBehaviour
+{
+    public float lerpDuration = 1f;
+    public float elapsed = 1f;
+
+    private float endX;
+    private float endY;
+    public bool lerpObject;
+    public float currentXPos;
+    public float currentYPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentXPos = transform.position.x;
+        currentYPos = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lerpObject == true)
+        {
+            if (elapsed < lerpDuration)
+            {
+                currentXPos = Mathf.Lerp(currentXPos, endX, elapsed / lerpDuration);
+                currentYPos = Mathf.Lerp(currentYPos, endY, elapsed / lerpDuration);
+                elapsed += Time.deltaTime;
+            }
+            else
+            {
+                currentXPos = endX;
+                currentYPos = endY;
+
+                lerpObject = false;
+            }
+        }
+
+        
+
+        transform.position = new Vector2(currentXPos, currentYPos);
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SetNewLerp(3, 0);
+        }
+    }
+
+
+    public void SetNewLerp(float newEndX, float newEndY)
+    {
+        endX = newEndX;
+        endY = newEndY;
+
+        elapsed = 0;
+
+        lerpObject = true;
+    }
+
+}
--- Array_Game/Assets/Scripts/GameManager.cs
+++ Array_Game/Assets/Scripts/GameManager.cs
@@ -4,33 +4,70 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject RockCard;
-    public GameObject PaperCard;
-    public GameObject ScissorCard;
+    private Vector2 target;
+    private float speed;
+
+    public Vector3 enemyPos1;
+    public Vector3 enemyPos2;
+    public Vector3 enemyPos3;
+
+    public Vector2 playerPos1;
+    public Vector2 playerPos2;
+    public Vector2 playerPos3;
+
+    public bool dealEnemy;
+    bool dealPlayer;
+    private GameObject CardOne;
+    private GameObject CardTwo;
+    private GameObject CardThree;
+    
+    public Vector3 EnemyPos1;
+    public Vector3 EnemyPos2;
+    public Vector3 EnemyPos3;
+
 
     public GameObject [] deck = new GameObject [10];
 
-    
+    private enum TurnStates
+    {
+        Shuffle,
+        MoveCards,
+        RevealCards,
+        Compare,
+    }
 
+    TurnStates turnStates;
 
     // Start is called before the first frame update
     void Start()
     {
+
         ShuffleDeck(deck);
+
+        dealEnemy = true;
+
+        speed = 5f;
    
     }
 
     // Update is called once per frame
     void Update()
     {
+      
         
         
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            DealCards();
+        }
 
     }
 
-
     private void ShuffleDeck(GameObject[] deck)
     {
+
+
         for (int cardIndex = 0; cardIndex < deck.Length; cardIndex++)
         {
             Debug.Log(deck[cardIndex].name);
@@ -43,13 +80,53 @@ public class GameManager : MonoBehaviour
 
         }
 
-
-    }
-
-
-    private void MoveCards()
-    {
         
+
     }
+
+    private void DealCards()
+    {
+
+        if (dealEnemy)
+        {
+            CardOne = deck[0];
+            CardTwo = deck[1];
+            CardThree = deck[2];
+
+            if (CardOne.transform.position != EnemyPos1)
+            {
+                CardOne.GetComponent<CardMovement>().SetNewLerp(enemyPos1.x, enemyPos1.y);
+            }
+            else if (CardTwo.transform.position != EnemyPos2)
+            {
+                CardTwo.GetComponent<CardMovement>().SetNewLerp(enemyPos2.x, enemyPos2.y);
+            }
+            else if (CardThree.transform.position != EnemyPos3)
+            {
+                CardThree.GetComponent<CardMovement>().SetNewLerp(enemyPos3.x, enemyPos3.y);
+            }
+        
+
+        }
+
+        //for (int x = 0; x <deck.Length; x++)
+        //{
+        //    if (x == 0)
+        //    {
+        //        deck[x].GetComponent<CardMovement>().SetNewLerp(enemyPos1.x, enemyPos1.y);
+        //    }
+        //    else if (x == 1)
+        //    {
+        //        deck[x].GetComponent<CardMovement>().SetNewLerp(enemyPos2.x, enemyPos2.y);
+        //    }
+        //    else if (x == 2)
+        //    {
+        //        deck[x].GetComponent<CardMovement>().SetNewLerp(enemyPos3.x, enemyPos3.y);
+        //    }
+        //}
+    }
+
+
+ 
 
 }
